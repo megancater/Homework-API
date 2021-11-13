ce2b9781438aa7b51657d9dab252bff0f038a84a
legitTomFoolery <ckorndor.jr@gmail.com>
2021-11-13 16:26:23 -0500

    index

--- HomeworkTracker/homework_api/frontend/src/index.js
+++ HomeworkTracker/homework_api/frontend/src/index.js
@@ -11,12 +11,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-App.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
-
-App.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
