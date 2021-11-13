7e953fad82a601c57c4c7f7c322f7fca0f256a55
cgillila <cgillila@nd.edu>
2021-11-13 14:40:10 -0500

    add requirements

--- HomeworkTracker/HomeworkTracker/settings.py
+++ HomeworkTracker/HomeworkTracker/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,3 +134,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 15
 }
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
